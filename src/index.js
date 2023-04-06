13b131e9e9ad10147aa432d994ba2e1c4b362d01
Charles Lao <charles.h.lao@gmail.com>
2023-04-06 21:25:31 +0800

    Set up webpack and create elements for header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-alert('Connected to js');
+import './style.css';
+import './reset-css.css';
+// import Background from './livestock-pattern.jpg';
+
+const displayController = (() => {
+
+    const container = document.querySelector("#content");
+    
+    const header = document.createElement("div");
+    header.setAttribute('id', 'header');
+    
+    const headerUl = document.createElement("ul");
+    
+    const homeLi = document.createElement("li");
+    homeLi.textContent = 'Home';
+
+    const menuLi = document.createElement("li");
+    menuLi.textContent = 'Menu';
+
+    const contactLi = document.createElement("li");
+    contactLi.textContent = "Contact"
+
+    
+    homeLi.addEventListener('click', (e) => {
+
+    });
+    
+    headerUl.appendChild(homeLi);
+    headerUl.appendChild(menuLi);
+    headerUl.appendChild(contactLi);
+
+    header.appendChild(headerUl);
+    container.appendChild(header);
+
+    
+})();
+
